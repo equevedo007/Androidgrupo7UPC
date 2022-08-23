26f06458635d5d353457434d3e36290a69e7a86c
EDINSON-LAPTOP <quevedo.edinson@gmail.com>
2022-08-23 18:33:43 -0500

    Entrega del Manu Principal 23/08/2022

--- app/src/main/java/com/example/androidgrupo7upc/Login_Activity.java
+++ app/src/main/java/com/example/androidgrupo7upc/Login_Activity.java
@@ -2,7 +2,9 @@ package com.example.androidgrupo7upc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class Login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void procesar (View v){
+        startActivity(new Intent(this, menu_Activity.class));
+    }
+
+
 }
