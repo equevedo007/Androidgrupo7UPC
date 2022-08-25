c33bd31f5888e8b58b84a7b98f9aa9547721e23e
EDINSON-LAPTOP <quevedo.edinson@gmail.com>
2022-08-25 16:33:16 -0500

    Entrega 02 -  25/08/2022

--- app/src/main/java/com/example/androidgrupo7upc/VisualizarUsuarios_Activity.java
+++ app/src/main/java/com/example/androidgrupo7upc/VisualizarUsuarios_Activity.java
@@ -16,12 +16,12 @@ public class VisualizarUsuarios_Activity extends AppCompatActivity {
 
         int i = 0;
         String[] matriz = new String[6];
-        matriz[i++] = "Pop";
-        matriz[i++] = "Punk";
-        matriz[i++] = "Música Clásica";
-        matriz[i++] = "Opera";
-        matriz[i++] = "Rock";
-        matriz[i++] = "Salsa";
+        matriz[i++] = "Usuario 01";
+        matriz[i++] = "Usuario 02";
+        matriz[i++] = "Usuario 03";
+        matriz[i++] = "Usuario 04";
+        matriz[i++] = "Usuario 05";
+        matriz[i++] = "Usuario 06";
 
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(VisualizarUsuarios_Activity.this,
                 android.R.layout.simple_list_item_1,
