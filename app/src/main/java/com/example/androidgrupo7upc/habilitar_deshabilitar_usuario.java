47aef1cbfa768615ffac66dbc59e378813fd1a69
Fatima <fatrive@gmail.com>
2022-08-25 09:37:13 -0500

    Creacion ventana actualizar paciente

---
+++ app/src/main/java/com/example/androidgrupo7upc/habilitar_deshabilitar_usuario.java
@@ -0,0 +1,14 @@
+package com.example.androidgrupo7upc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class habilitar_deshabilitar_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habilitar_deshabilitar_usuario);
+    }
+}
