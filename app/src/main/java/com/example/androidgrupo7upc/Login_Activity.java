1130685000ef53f351d518accca2a0dcf1353cd0
EDINSON-LAPTOP <quevedo.edinson@gmail.com>
2022-09-14 15:20:56 -0500

    Entrega 01– 14/09/2022

--- app/src/main/java/com/example/androidgrupo7upc/Login_Activity.java
+++ app/src/main/java/com/example/androidgrupo7upc/Login_Activity.java
@@ -5,8 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Login_Activity extends AppCompatActivity {
+
+
+    //private Object StringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +38,47 @@ public class Login_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void procesar (View v){
-       // startActivity(new Intent(this, menu_Activity.class));
+
+
+    public void login (View v) {
+
+
+        /*final EditText txtUsuario = findViewById(R.id.editTextTextPersonName);
+
+        final EditText txtPassword = findViewById(R.id.editTextTextPassword2);
+
+        String url = "http://139.144.33.83:8080/opeviso-api/v1/login";
+
+       StringRequest stringRequest= new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast toast = Toast.makeText(Login_Activity.this,"Se insertó correctamente", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("======>", error.toString());
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap();
+                params.put("nombre", txtUsuario.getText().toString());
+                params.put("precio", txtPassword.getText().toString());
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);*/
 
         startActivity(new Intent(this, menu_Activity.class));
+        }
     }
 
 
-}
---
+++ app/src/main/java/com/example/androidgrupo7upc/webservices.java
@@ -0,0 +1,50 @@
+package com.example.androidgrupo7upc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class webservices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webservices);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
